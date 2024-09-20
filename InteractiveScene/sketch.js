0b60b1e58f602729c100db929fc9000f344c517a
Pletmintsev7009529 <112390158+OlegPlet19@users.noreply.github.com>
2024-09-20 11:03:08 -0600

    InitialCommit
    Circle moves around screen

--- InteractiveScene/sketch.js
+++ InteractiveScene/sketch.js
@@ -5,6 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let circleX = 400;
+let circleY = 480;
+
+let circleSpeed = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +16,15 @@ function setup() {
 
 function draw() {
   background(220);
+
+  circle(circleX, circleY, 50)
+
+  if (keyIsDown(LEFT_ARROW)){
+      circleX = circleX - circleSpeed;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)){
+    circleX = circleX + circleSpeed;
+  }
+
 }
