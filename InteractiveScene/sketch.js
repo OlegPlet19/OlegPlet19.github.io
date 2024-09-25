1bbf53adf91255655f045702d91a62e1338a6bf8
Oleg <ollegga2007@gmail.com>
2024-09-25 11:02:23 -0600

    Update sketch.js

--- InteractiveScene/sketch.js
+++ InteractiveScene/sketch.js
@@ -5,47 +5,78 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let circleX = 300;
-let circleY = 200;
-let circleSize = 50;
+// Ball
+let ball_x = 200;
+let ball_y = 200;
 
-let squareX = 600;
-let squareY = 175;
+let ball_dx = 4;
+let ball_dy= 2;
 
-// left corner
-let triangeX1 = 900;
-let triangeY1 = 250;
-// right corner
-let triangeX2 = 1100;
-let triangeY2 = 250;
-// top corner
-let triangeX3 = 1000;
-let triangeY3 = 100;
+let ballRadius = 30;
+let r = 0;
+let g = 255;
+let b = 255;
 
-//let quadX1 = 0;
-//let quadY1 = 0;
-
+// player 1
+let playerX = 50;
+let playerY = 250;
+let playerHeight = 100;
+let playerWidth = 25;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 500);
 }
 
 function draw() {
   background(220);
+  
+  moveBall();
+  bounceBall();
+  displayBall();
+  displayPlayer1();
 
-  circle(circleX, circleY, circleSize)
-
-  square(squareX, squareY, 50)
-
-  triangle(triangeX1, triangeY1, triangeX2, triangeY2, triangeX3, triangeY3)
- 
-  //quad(50, 62, 86, 50, 50, 38, 14, 50);
-
-  if (mouseX && mouseY === circleX, circleY){
-    if (mouseIsPressed === true){
-      circle(mouseX, mouseY, circleSize);
-      //if (mouseWheel )
-    }
+  if (keyIsDown(87) && playerY > 0){
+    playerY -= 5; 
   }
+  else if (keyIsDown(83) && playerY < 500 - playerHeight){
+    playerY += 5;
+  } 
+}
 
+function moveBall() {
+  //move
+  ball_x += ball_dx;
+  ball_y += ball_dy;
+}
+
+function bounceBall() {
+  // xhecking collision with player
+  if (ball_x === playerX - playerWidth)
+
+  //bounce if needed
+  if (ball_x >= width - ballRadius || ball_x <= 0 + ballRadius) {
+    ball_dx = ball_dx * -1;
+    pickRandomColor();
+  }
+  if (ball_y >= height - ballRadius || ball_y <= 0 + ballRadius) {
+    ball_dy = ball_dy * -1;
+    pickRandomColor();
+  }
+}
+
+function pickRandomColor() {
+  r = random(255);
+  g = random(255);
+  b = random(255);
+}
+
+function displayBall() {
+  //display
+  fill(r, g, b);
+  circle(ball_x, ball_y, ballRadius * 2);
+}
+
+function displayPlayer1(){
+  fill("black");
+  rect(playerX, playerY, playerWidth, playerHeight);
 }
