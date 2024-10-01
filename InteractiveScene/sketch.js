318edfcf32487c69cea38949f8854cf07d502543
Oleg <112390158+OlegPlet19@users.noreply.github.com>
2024-09-30 22:59:19 -0600

    Beta Version

--- InteractiveScene/sketch.js
+++ InteractiveScene/sketch.js
@@ -1,13 +1,13 @@
-// Project Title
-// Your Name
-// Date
+// A simple adaptation of the table tennis game
+// Oleh Pletmintsev
+// 30.09.2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Lists for using random explored and used, no cursor on game canvas, text on game canvas.
 
 // Ball
-let ball_x = 200;
-let ball_y = 200;
+let ball_x = 960;
+let ball_y = 540;
 
 let ball_dx = 4;
 let ball_dy= 2;
@@ -22,28 +22,41 @@ let playerOneX = 50;
 let playerOneY = 250;
 
 //player 2
-let playerTwoX = 750;
+let playerTwoX = 1800;
 let playerTwoY = 250;
 
 let playerHeight = 100;
 let playerWidth = 25;
 
+// Debug
 let bounceSideRight = true;
 
+// Statistics
 let playerOneScore = 0;
 let playerTwoScore = 0;
 
 let speedIncrease = 1;
 
+let ballXDirection = [-5, -4, -3, 3, 4, 5];
+let ballYDirection = [-2, -1, 1, 2];
+
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(windowWidth, windowHeight);
+
+  ball_x = windowWidth/2;
+  ball_y = windowHeight/2;
+
+  ball_dx = random(ballXDirection);
+  ball_dy = random(ballYDirection);
+  noCursor();
 }
 
 function draw() {
+
   background(220);
 
   text("Player 1 score: " + playerOneScore, 100, 50);
-  text("Player 2 score: " + playerTwoScore, 650, 50);
+  text("Player 2 score: " + playerTwoScore, windowWidth - 150, 50);
   
   moveBall();
   bounceBall();
@@ -55,7 +68,7 @@ function draw() {
   if (keyIsDown(87) && playerOneY > 0){
     playerOneY -= 5; 
   }
-  else if (keyIsDown(83) && playerOneY < 500 - playerHeight){
+  else if (keyIsDown(83) && playerOneY < windowHeight - playerHeight){
     playerOneY += 5;
   } 
 
@@ -63,7 +76,7 @@ function draw() {
   if (keyIsDown(UP_ARROW) && playerTwoY > 0){
     playerTwoY -= 5; 
   }
-  else if (keyIsDown(DOWN_ARROW) && playerTwoY < 500 - playerHeight){
+  else if (keyIsDown(DOWN_ARROW) && playerTwoY < windowHeight - playerHeight){
     playerTwoY += 5;
   } 
 }
@@ -75,11 +88,28 @@ function moveBall() {
 }
 
 function bounceBall() {
+  if (ball_dx > 0){
+    bounceSideRight = true;
+  } else {
+    bounceSideRight = false;
+  }
+
   // Checking collision with player
   if (!bounceSideRight){
+    // Checking collision with wall
+    if (ball_x < playerOneX){
+      console.log("Game end");
+
+      text("Player 2 Won! Update the page to play again.", windowWidth/2, windowHeight/2);
+
+      pause();
+    }
+
+    // Checking X-coordinate of ball to collide with moving brick
     if (ball_x <= playerOneX + ballRadius + playerWidth){
       console.log("On line of hit with player 1");
 
+      // Checking Y-coordinate of ball to collide with moving brick
       if (ball_y >= playerOneY && ball_y <= playerOneY + playerHeight) {
         console.log("bounce");
 
@@ -94,9 +124,20 @@ function bounceBall() {
     }
   }
   else{
+    // Checking collision with wall
+    if (ball_x > playerTwoX + playerWidth){
+      console.log("Game end");
+
+      text("Player 1 Won! Update the page to play again.", windowWidth/2, windowHeight/2);
+
+      pause();
+    }
+    
+    // Checking X-coordinate of ball to collide with moving brick
     if (ball_x >= playerTwoX - ballRadius){
       console.log("On line of hit with player 2");
 
+      // Checking Y-coordinate of ball to collide with moving brick
       if (ball_y >= playerTwoY && ball_y <= playerTwoY + playerHeight) {
         console.log("bounce");
 
@@ -111,8 +152,6 @@ function bounceBall() {
     }  
   }
 
-  
-
   // Bounce if needed
   if (ball_x >= width - ballRadius || ball_x <= 0 + ballRadius) {
     ball_dx = ball_dx * -1 * speedIncrease;
@@ -122,7 +161,7 @@ function bounceBall() {
 
   // Bounce if needed
   if (ball_y >= height - ballRadius || ball_y <= 0 + ballRadius) {
-    ball_dy = ball_dy * -1 * speedIncrease;
+    ball_dy = ball_dy * -1  * speedIncrease;
     pickRandomColor();
   }
 }
@@ -151,4 +190,5 @@ function displayPlayer1(){
 function displayPlayer2(){
   fill("black");
   rect(playerTwoX, playerTwoY, playerWidth, playerHeight);
+  playerTwoX = windowWidth - playerOneX - playerWidth;
 }
