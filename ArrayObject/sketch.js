997fc891fd937e89ee09ffcb6fc80b6adf4fcf6b
Oleg <112390158+OlegPlet19@users.noreply.github.com>
2024-10-22 03:25:40 -0600

    Sound Effects

--- ArrayObject/sketch.js
+++ ArrayObject/sketch.js
@@ -3,17 +3,28 @@
 // 08/10/2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// learned a the ternary operator in JavaScript: return laserChance < 50 ? theLaser.vertical : theLaser.horizontal;
+// Sound effects
 
 let player;
+let layerHit = false;
+
 let wall;
 let walls = [];
-let laser;
-let timeBetweenLasers = 2;
-let currentTimeBetweenLasers = 2;
-let groundHeight = 100;
 let amountOfWalls = 5;
 let minDistanceBetweenWalls = 200;
+
+let laserWeight = 0;
+let laser;
+let timeBetweenLasers = 0.5;
+let currentTimeBetweenLasers = 0.5;
+
+let groundHeight = 100;
+
+function preload(){
+  bounceFx = loadSound("Jump.mp3");
+  endGameFx = loadSound("EndGame.wav");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -30,6 +41,7 @@ function setup() {
 
     walls.push(newWall);
   }
+  laser  = spawnLaser();  
 }
 
 function draw() {
@@ -44,17 +56,20 @@ function draw() {
   applyGravity();
   playerMovement();
   checkCollision();
+  checkCollisionWithLaser();
 }
 
 // Draw Player
 function displayPlayer() {
   // Player color stands green when able to jump and red when not
-  if (player.isJumping){
+  if (player.isJumping) {
     fill("red");
-  } else  {
+  } else {
     fill("green");
   }
   
+  strokeWeight(1);
+  stroke("black");
   rect(player.x, player.y, player.width, player.height);
 }
 
@@ -66,18 +81,53 @@ function displayWalls() {
   }
 }
 
-// Draw Line
+// Draw Line (ground)
 function displayLine() {
   line(0, height - groundHeight, width, height - groundHeight);
 }
 
+// Function that displayes laser
 function displayLaser() {
   currentTimeBetweenLasers -= 0.01;
-  if (currentTimeBetweenLasers <= 0) {
-    let theLaser = spawnLaser();
-    line(theLaser.x1, theLaser.x2, theLaser.y1, theLaser.y2);
-    console.log("creating laser");
+
+// Check if time has passed between laser movements
+if (currentTimeBetweenLasers <= 0) {
+// Check if the player is inside the current laser before moving it
+  if (
+      (laser.x1 === laser.x2 && // If laser is vertical
+        player.x + player.width >= laser.x1 &&
+        player.x <= laser.x1 &&
+        player.y + player.height >= laser.y1 &&
+        player.y <= laser.y2) ||
+      (laser.y1 === laser.y2 && // If laser is horizontal
+        player.y + player.height >= laser.y1 &&
+        player.y <= laser.y1 &&
+        player.x + player.width >= laser.x1 &&
+        player.x <= laser.x2)
+    ) {
+      // If the player is in the laser, pause the game
+      console.log("GAME OVER!!!!");
+      endGameFx.play();
+      noLoop(); // Stop the game
+    } else {
+      // Move the laser to a new position
+      laser = spawnLaser();
+    }
+
+    // Reset the current time between lasers
+    currentTimeBetweenLasers = timeBetweenLasers;
   }
+
+  // Updating the weight of laser's stroke
+  if (laserWeight >= 10) {
+    laserWeight = 0;
+  }
+  laserWeight += 0.2;
+  strokeWeight(laserWeight);
+  stroke('red');
+
+  // Draw laser
+  line(laser.x1, laser.y1, laser.x2, laser.y2);
 }
 
 function spawnPlayer() {
@@ -99,6 +149,7 @@ function spawnPlayer() {
   return player;
 }
 
+// Function that sets and returns wall's values
 function spawnWalls() {
   let wallHeight = random(75, 150); // Height of the wall
   let wallWidth = random(75, 150);   // Width of  the wall
@@ -113,35 +164,35 @@ function spawnWalls() {
   };
 }
 
+// Function that sets and returns laser's values
 function spawnLaser() {
   let minDistanceOfLaser = 50;
+
+  let x1 = random(minDistanceOfLaser, width - minDistanceOfLaser); // Setting up x1 for vertical laser 
+  let y1 = random(minDistanceOfLaser, height - minDistanceOfLaser - groundHeight); // Setting up y1 for horizontal laser 
+
   theLaser = {
     vertical : { 
       y1 : 0,
       y2 : height,
-      x1 : random(minDistanceOfLaser, width - minDistanceOfLaser),
-      x2 : x1,
+      x1 : x1,
+      x2: x1, 
     },
 
     horizontal : {
       x1 : 0,
       x2 : width,
-      y1 : random(minDistanceOfLaser, height - minDistanceOfLaser),
-      y2 : y1,
+      y1 : y1,
+      y2: y1,
     },
   };
 
   let laserChance = random(100);
 
-  // if (laserChance < 50) {
-  //   return theLaser.vertical;
-  // } else {
-  //   return theLaser.horizontal;
-  // }
-
-  return laserChance < 50? theLaser.vertical : theLaser.horizontal;
+  return laserChance < 50 ? theLaser.vertical : theLaser.horizontal;
 }
 
+// Function that moves player 
 function playerMovement() {
   //Move right
   if (keyIsDown(68) && player.x < width - player.width) {
@@ -182,14 +233,15 @@ function playerMovement() {
   }
 
   // Jumping is possible if the player is on the ground or on a wall
-  if (/*(player.onGround || player.onWall) &&*/ keyIsDown(32) && !player.isJumping) {
+  if (keyIsDown(32) && !player.isJumping) {
     player.velocity = player.jumpHeight;
     player.isJumping = true;
-    player.onWall = false; 
-    console.log("Player Jumped: " + player.isJumping);
+    player.onWall = false;
+    bounceFx.play();
   }
 }
 
+// Function that creates gravity
 function applyGravity() {
   player.y += player.velocity;
   player.velocity += player.gravity;
@@ -197,12 +249,11 @@ function applyGravity() {
   if (player.y + player.height + groundHeight > height) {
     player.y = height - player.height - groundHeight;
     player.velocity = 0;
-    console.log("On ground: " + player.onGround);
   }
 }
 
+// Check if the player is standing on the ground or on a wall
 function isOnGroundOrWall() {
-  // Check if the player is standing on the ground or on a wall
   player.onGround = player.y + player.height + groundHeight >= height;
   player.onWall = false; 
 
@@ -223,7 +274,7 @@ function isOnGroundOrWall() {
     }
   }
 
-  if (player.onGround/* || player.onWall*/) {
+  if (player.onGround) {
     player.isJumping = false;
   }
 }
@@ -262,6 +313,7 @@ function isValidWallPosition(newWall) {
   return true; 
 }
 
+// Fucntion that checks collison between player and wall
 function checkCollision() {
   player.onGround = false; // Imagine player on ground
 
@@ -279,8 +331,32 @@ function checkCollision() {
       player.velocity = 0;
       player.isJumping = false;
       player.onGround = true;
-      console.log("Player is jumping :" + player.isJumping);
-      console.log("Player on ground :" + player.onGround);
+    }
+  }
+}
+
+function checkCollisionWithLaser() {
+  // Vertical laser collision check
+  if (laser.x1 === laser.x2) { // Vertical laser
+    if (
+      player.x + player.width >= laser.x1 &&  // Player's right side crosses x1 laser
+      player.x <= laser.x1 &&                 // Player's left side crosses x1 laser
+      player.y + player.height >= laser.y1 && // Player's bottom is below the laser's top border
+      player.y <= laser.y2                    // The top of the player is above the bottom of the laser
+    ) {
+      playerHit = true; // Player was hit by the laser
+    }
+  }
+
+  // Check for collision with horizontal laser
+  if (laser.y1 === laser.y2) { // Horizontal laser
+    if (
+      player.y + player.height >= laser.y1 &&  // Player's bottom intersects y1 laser
+      player.y <= laser.y1 &&                  // The player's top crosses the y1 laser
+      player.x + player.width >= laser.x1 &&   // The player's right side is behind the start of the laser
+      player.x <= laser.x2                     // Player's left side before the end of the laser
+    ) {
+      playerHit = true; // Player was hit by a laser
     }
   }
 }
