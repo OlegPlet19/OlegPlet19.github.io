251b9d8353e9848f29ee11ec482cc807082c60dd
Oleg <112390158+OlegPlet19@users.noreply.github.com>
2024-11-01 01:37:56 -0600

    Working Logic For 2  Players
    Need to add neural net by next step to  have smth like "AI" in game.

--- TicTacToe/sketch.js
+++ TicTacToe/sketch.js
@@ -20,22 +20,9 @@ function setup() {
   cellSize = height/GRID_SIZE;
 }
 
-// Creating changeble screen
-function windowResized() {
-  if (windowWidth < windowHeight) {
-    resizeCanvas(windowWidth, windowWidth);
-  }
-  else {
-    resizeCanvas(windowHeight, windowHeight);
-  }
-  cellSize = height/GRID_SIZE;
-}
-
 function draw() {
   background(220);
   displayGrid();
-  // checkThreeInARow();
-  // checkTie();
 }
 
 // Getting the x and y pos 
@@ -43,12 +30,12 @@ function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
-  //toggle self
+  // Toggle cell and check for winner or tie after each move
   toggleCell(x, y);
 }
 
 function toggleCell(x, y) {
-  //make sure the cell you're toggling is in the grid
+  // Make sure the cell you're toggling is in the grid
   if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) { // To not get off the screen
     if (grid[y][x] === 0 && playerTurn) { // Player X
       grid[y][x] = 1;
@@ -58,8 +45,8 @@ function toggleCell(x, y) {
       grid[y][x] = 2;
       playerTurn = !playerTurn;
     }
+    checkForWinner();
   }
-  checkWin(x, y);
 }
 
 function displayGrid() {
@@ -79,48 +66,51 @@ function displayGrid() {
   }
 }
 
-function checkWin(x, y) {
-  if (grid[y][x] === 2) {
-    console.log("that spot is green, y: " + y + ", and x: " + x);
+function checkThreeInARow() {
+  for (let i = 0; i < 3; i++) {
+    // Check for X win conditions
+    if (grid[0][i] === 1 && grid[1][i] === 1 && grid[2][i] === 1 || 
+        grid[i][0] === 1 && grid[i][1] === 1 && grid[i][2] === 1 || 
+        grid[0][0] === 1 && grid[1][1] === 1 && grid[2][2] === 1 || 
+        grid[0][2] === 1 && grid[1][1] === 1 && grid[2][0] === 1
+    ) { 
+      return 1;
+    }
+    // Check for O win conditions
+    else if (grid[0][i] === 2 && grid[1][i] === 2 && grid[2][i] === 2 || 
+            grid[i][0] === 2 && grid[i][1] === 2 && grid[i][2] === 2 ||
+            grid[0][0] === 2 && grid[1][1] === 2 && grid[2][2] === 2 || 
+            grid[0][2] === 2 && grid[1][1] === 2 && grid[2][0] === 2
+    ) { 
+      return 2;
+    }
   }
-  if (grid[y][x] === 1) {
-    console.log("that spot is black, y: " + y + ", and x: " + x);
-  }
+  return 0; // No winner
 }
 
-// function checkThreeInARow() {
-//   for (let i = 0; i < 3; i++) {
-//     if (grid[0][i] === 1 && grid[1][i] === 1 && grid[2][i] === 1 || 
-//         grid[i][0] === 1 && grid[i][1] === 1 && grid[i][2] === 1 || 
-//         grid[0][0] === 1 && grid[1][1] === 1 && grid[2][2] === 1 || 
-//         grid[0][2] === 1 && grid[1][1] === 1 && grid[2][0] === 1
-//     ) { 
-//       return true;
-//     }
-//     else if (grid[0][i] === 2 && grid[1][i] === 2 && grid[2][i] === 2 || 
-//             grid[i][0] === 2 && grid[i][1] === 2 && grid[i][2] === 2 ||
-//             grid[0][0] === 2 && grid[1][1] === 2 && grid[2][2] === 2 || 
-//             grid[0][2] === 2 && grid[1][1] === 2 && grid[2][0] === 2
-//     ) { 
-//       return true;
-//     }
-//   }
-// }
+function checkTie() {
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+      if (grid[y][x] === 0) {
+        return false; // There are still empty spots
+      }   
+    }
+  }
+  return true; // All spots are filled
+}
 
-// function checkTie() {
-//   // check whether all squares are full and nobody has 3 in a row
-//   for (let y = 0; y < 3; y++) {
-//     for (let x = 0; x < 3; x++) {
-//       if (grid[y][x] === 0) {
-//         return false;
-//       }   
-//     }
-//   }
-
-//   if (checkThreeInARow()) {
-//     console.log("X wins");
-//     return false;
-//   }
-//   console.log("O wins");
-//   return true;
-// }
+function checkForWinner() { 
+  let winner = checkThreeInARow();
+  if (winner === 1) {
+    console.log("Player X wins!");
+    noLoop(); // Stop the draw loop to prevent further moves
+  }
+  else if (winner === 2) {
+    console.log("Player O wins!");
+    noLoop(); // Stop the draw loop to prevent further moves
+  }
+  else if (checkTie()) {
+    console.log("It's a tie!");
+    noLoop(); // Stop the draw loop to prevent further moves
+  }
+}
